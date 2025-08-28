e042cf644b63db0904bbd6ce3924d4978c58a46c
pabmevi <pabmevi@gmail.com>
2025-08-28 18:35:40 -0500

    new

--- data/roadkill/roadkill_allrates.py
+++ data/roadkill/roadkill_allrates.py
@@ -2,13 +2,13 @@ import numpy as np
 from foldrm import Classifier
 
 def Imput_lessnoise_allsp_rates():
-    attrs = ["Frequency_month","Survey_interval_days","Road_length_km","Survey_period_days","Latitude","Longitude","Country",
+    attrs = ["Frequency_month","Survey_interval_days","Road_length_km","Survey_period_days","Latitude","Longitude",
              "AdultBodyMass_g_median","Home_range_Km2","longevity_y","Ageofmaturity_d",
-             "SocialGrpSize","Diet_Invertebrates","Diet_Vertebrates.ectotherms","Diet_Scavenger","Diet_Seed","Diet_Plant","Activity_1Diurnal_2Nocturnal",
+             "Diet_Invertebrates","Diet_Vertebrates.ectotherms","Diet_Scavenger","Diet_Seed","Diet_Plant","Activity_1Diurnal_2Nocturnal",
              "Litter_clutch_size","Litters_or_clutches_per_y","Diet_breadth","Artificial","Cropland","Grassland","Forest","Sparse.vegetation","Water.bodies",
              "Habitat_breadth","Population.density_IndKm2"]
     nums = ["Frequency_month","Survey_interval_days","Road_length_km","Survey_period_days","Latitude","Longitude","AdultBodyMass_g_median",
-            "Home_range_Km2","longevity_y","Ageofmaturity_d","SocialGrpSize","Litter_clutch_size","Litters_or_clutches_per_y","Diet_breadth","Habitat_breadth","Population.density_IndKm2"]
+            "Home_range_Km2","longevity_y","Ageofmaturity_d","Litter_clutch_size","Litters_or_clutches_per_y","Diet_breadth","Habitat_breadth","Population.density_IndKm2"]
     model = Classifier(attrs=attrs, numeric=nums, label='risk_category')
     data = model.load_data('data/roadkill/Imput_lessnoise_allsp_rates_classified.csv')
     print('\n% roadkill dataset', np.shape(data))
