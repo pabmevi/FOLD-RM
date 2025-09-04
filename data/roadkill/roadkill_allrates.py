819e10dcc4ec5ee88947190c0735512ec6f5e510
pabmevi <pabmevi@gmail.com>
2025-09-04 14:19:13 -0500

    4

--- data/roadkill/roadkill_allrates.py
+++ data/roadkill/roadkill_allrates.py
@@ -19,9 +19,9 @@ def Imput_lessnoise_allsp_rates():
 
 model, data = Imput_lessnoise_allsp_rates()
 
-# Separar datos en entrenamiento y test (80% train, 20% test)
+# Separar datos en entrenamiento y test (90% train, 10% test)
 from utils import split_data
-train_data, test_data = split_data(data, ratio=0.8, shuffle=True)
+train_data, test_data = split_data(data, ratio=0.9, shuffle=True)
 
 # Entrenar solo con el set de entrenamiento
 model.fit(train_data, ratio=0.9)
