a9f34890ddcd42f5fe7a5aa041693428119080b5
pabmevi <pabmevi@gmail.com>
2025-09-05 12:51:39 -0500

    v

--- data/roadkill/roadkill_allrates.py
+++ data/roadkill/roadkill_allrates.py
@@ -30,7 +30,7 @@ train_data, test_data = split_data(data, ratio=0.9, shuffle=True)
 
 # Entrenar solo con el set de entrenamiento
 model.fit(train_data, ratio=0.9)
-model.confidence_fit(train_data, improvement_threshold=0.1)
+model.confidence_fit(train_data, improvement_threshold=1)
 
 
 print("\nLearned Answer Set Program rules:\n")
