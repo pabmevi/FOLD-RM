3bc6179fd9a8a76c8fb171110ee170f1b5f7b834
pabmevi <pabmevi@gmail.com>
2025-09-05 12:31:58 -0500

    d

--- data/roadkill/roadkill_medianrates.py
+++ data/roadkill/roadkill_medianrates.py
@@ -34,7 +34,7 @@ train_data, test_data = split_data(data, ratio=0.9, shuffle=True)
 # ===========================
 # Entrenamiento
 # ===========================
-model.fit(train_data, ratio=0.7)
+model.fit(train_data, ratio=0.9)
 model.confidence_fit(train_data, improvement_threshold=0.9)
 
 print("\nLearned Answer Set Program rules:\n")
